708cb630b2dc0ec60243041626759e743f4904be
Chmouel Boudjnah <chmouel@chmouel.com>
2011-07-07 16:39:40 +0100

    0.4 release.

--- cloudlb/consts.py
+++ cloudlb/consts.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 __author__ = "Chmouel Boudjnah <chmouel@chmouel.com>"
-VERSION = "0.3"
+VERSION = "0.4"
 USER_AGENT = 'python-cloudb/%s' % VERSION
 
 # Default AUTH SERVER
