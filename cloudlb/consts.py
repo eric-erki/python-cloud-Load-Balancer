cf160bf30453df77bf970383b2ab548a61226d93
Jason Straw <jason.straw@rackspace.com>
2012-06-02 19:47:21 -0500

    Fix Version Number

--- cloudlb/consts.py
+++ cloudlb/consts.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 __author__ = "Chmouel Boudjnah <chmouel@chmouel.com>"
-VERSION = "0.4"
+VERSION = "0.5.1"
 USER_AGENT = 'python-cloudb/%s' % VERSION
 
 # Default AUTH SERVER
